7da3fdf3fbea781b37d8c7ddbcdc0f0ca389c86c
raffled <raffled@gmail.com>
2015-08-02 19:22:40 -0400

    used roxy for coag description

---
+++ R/coag_descript.R
@@ -0,0 +1,15 @@
+#' Blood coagulation times by diet
+#'
+#' Dataset comes from a study of blood coagulation times. 24 animals were
+#' randomly assigned to four different diets and the samples were taken
+#' in a random order.
+#'
+#' @format a data frame with 24 rows and two columns:
+#'
+#' \describe{
+#'   \item{coag}{coagulation time in seconds}
+#'   \item{diet}{diet type: A, B, C, or D}
+#' }
+#' @source "Statistics for Experimenters" by G. P. Box, W. G. Hunter and J. S. Hunter, Wiley, 1978
+#'
+"coagulation"
